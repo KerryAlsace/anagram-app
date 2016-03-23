e321f43d4cc964a682947117c3b0c98cd79c6348
KerryAlsace <kerrydougherty@gmail.com>
2016-03-23 11:13:38 -0400

    add anagram algorithm

--- anagram.rb
+++ anagram.rb
@@ -1,6 +1,19 @@
 require_relative 'reverse_letters'
 
-def find_anagram(word)
-  letters = reverse_letters(letters)
-  (letters).each_cons(3).to_s
+def find_anagram(letters)
+  length = letters.length
+
+  # Make the letters an array
+  letters = letters.split("")
+
+  anagrams = []
+
+  letters.each_with_index do |letter, index|
+    remaining_letters = letters.select {|l| l != letter}
+    anagrams << letter + remaining_letters.join
+
+    anagrams << letter + reverse_letters(remaining_letters).join
+  end
+
+  anagrams
 end
